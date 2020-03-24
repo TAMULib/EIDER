18215004757eeab529c2a65054910003a47e99ca
Ryan Laddusaw <rladdusaw@gmail.com>
2020-03-24 10:23:18 -0500

    All endpoints working with links

--- src/main/java/edu/tamu/eider/app/web/EntityController.java
+++ src/main/java/edu/tamu/eider/app/web/EntityController.java
@@ -7,17 +7,17 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.BasePathAwareController;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.view.RedirectView;
 
 import edu.tamu.eider.app.model.Entity;
 import edu.tamu.eider.app.model.repo.EntityRepository;
 
-@RestController
+@BasePathAwareController
 public class EntityController {
 
     @Autowired
@@ -28,16 +28,6 @@ public class EntityController {
         Optional<Entity> entityOption = entityRepo.findById(uuid);
         if (entityOption.isPresent()) {
             return new RedirectView(entityOption.get().getUrl().toString());
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find Entity for provided id");
-        }
-    }
-
-    @RequestMapping(path = "/entity/{uuid}", method = GET)
-    public Entity getEntity(@PathVariable("uuid") UUID uuid) {
-        Optional<Entity> entityOption = entityRepo.findById(uuid);
-        if (entityOption.isPresent()) {
-            return entityOption.get();
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find Entity for provided id");
         }
