1932d789aba8c54d64a9c0a492ec054cd21cebcc
Ryan Laddusaw <rladdusaw@gmail.com>
2020-03-24 16:00:58 -0500

    Added findByUrl exposed ids

--- src/main/java/edu/tamu/eider/app/model/repo/EntityRepository.java
+++ src/main/java/edu/tamu/eider/app/model/repo/EntityRepository.java
@@ -1,13 +1,18 @@
 package edu.tamu.eider.app.model.repo;
 
+import java.net.URL;
 import java.util.UUID;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import edu.tamu.eider.app.model.Entity;
 
 @RepositoryRestResource(collectionResourceRel = "entity", path = "entity")
 public interface EntityRepository extends PagingAndSortingRepository<Entity, UUID> {
 
+    @RestResource(path = "findByUrl", rel = "findByUrl")
+    public Entity findByUrl(@PathVariable("url") URL url);
 }
