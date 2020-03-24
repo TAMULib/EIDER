7d73cba6881b66d5a79ea3402af4757517a3b1c3
Ryan Laddusaw <rladdusaw@gmail.com>
2020-03-24 15:37:11 -0500

    Cleanup

--- src/main/java/edu/tamu/eider/app/model/processor/EntityProcessor.java
+++ src/main/java/edu/tamu/eider/app/model/processor/EntityProcessor.java
@@ -2,10 +2,6 @@ package edu.tamu.eider.app.model.processor;
 
 import static org.springframework.hateoas.Link.of;
 
-import javax.servlet.ServletContext;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.LinkRelation;
 import org.springframework.hateoas.server.RepresentationModelProcessor;
@@ -15,19 +11,13 @@ import edu.tamu.eider.app.model.Entity;
 
 public class EntityProcessor implements RepresentationModelProcessor<EntityModel<Entity>> {
 
-    @Value("${server.port}")
-    private String port;
-
-    @Autowired
-    ServletContext context;
-
     @Override
     public EntityModel<Entity> process(EntityModel<Entity> model) {
-            String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
-            model.add(
-                of(baseUrl + "entity/{id}/redirect").withRel(LinkRelation.of("entity"))
-                    .expand(model.getContent().getId())
-            );
+        String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+        model.add(
+            of(baseUrl + "entity/{id}/redirect").withRel(LinkRelation.of("entity"))
+                .expand(model.getContent().getId())
+        );
         return model;
     }
 }
