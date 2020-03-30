17659c8a82e7d66c0767ca7842d2a7d1520d9ddc
Ryan Laddusaw <rladdusaw@gmail.com>
2020-03-30 08:41:49 -0500

    Refactored validators to remove redundancy

--- src/main/java/edu/tamu/eider/app/model/validator/BeforeSaveEntityValidator.java
+++ src/main/java/edu/tamu/eider/app/model/validator/BeforeSaveEntityValidator.java
@@ -1,33 +1,14 @@
 package edu.tamu.eider.app.model.validator;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.Errors;
-import org.springframework.validation.Validator;
 
 import edu.tamu.eider.app.model.Entity;
-import edu.tamu.eider.app.model.repo.EntityRepository;
-import edu.tamu.eider.app.model.repo.IdentifierRepository;
 
 @Component
-public class BeforeSaveEntityValidator implements Validator {
-
-    @Autowired
-    private EntityRepository entityRepo;
-
-    @Autowired
-    private IdentifierRepository identifierRepo;
+public class BeforeSaveEntityValidator extends UniqueUrlValidator {
 
     public boolean supports(Class<?> clazz) {
         return Entity.class.equals(clazz);
     }
 
-    public void validate(Object obj, Errors e) {
-        Entity entity = (Entity) obj;
-        if (entityRepo.existsByUrl(entity.getUrl())) {
-            e.rejectValue("url", "url.not.unique", "There is already an Entity with the given url");
-        } else if (identifierRepo.existsByIdentifier(entity.getUrl().toString())) {
-            e.rejectValue("url", "url.not.unique", "There is already an Identifier with the given url");
-        }
-    }
 }
