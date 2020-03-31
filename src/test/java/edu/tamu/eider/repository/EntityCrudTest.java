1b5c04ecddd02b2518b5332fcd7494ffe1813238
Ryan Laddusaw <rladdusaw@gmail.com>
2020-03-31 13:43:18 -0500

    Removed redundancy in tests and finished entity crud documentation

--- src/test/java/edu/tamu/eider/repository/EntityCrudTest.java
+++ src/test/java/edu/tamu/eider/repository/EntityCrudTest.java
@@ -26,6 +26,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.hypermedia.LinksSnippet;
+import org.springframework.restdocs.payload.RequestFieldsSnippet;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
 
 import edu.tamu.eider.ApiDocumentation;
 import edu.tamu.eider.app.model.Entity;
@@ -42,6 +45,36 @@ public class EntityCrudTest extends ApiDocumentation {
 
     private static final Entity TEST_ENTITY_1 = new Entity();
     private static final Entity TEST_ENTITY_2 = new Entity();
+
+    private static final RequestFieldsSnippet ENTITY_REQUEST_FIELDS_SNIPPET = requestFields(
+        fieldWithPath("url").description("The URL of the Entity"),
+        fieldWithPath("canonicalName").description("The canonical name for the Entity"),
+        fieldWithPath("notes").description("Notes describing the Entity")
+    );
+
+    private static final ResponseFieldsSnippet ENTITY_RESPONSE_FIELDS_SNIPPET = responseFields(
+        fieldWithPath("id").description("The UUID id of the Entity"),
+        fieldWithPath("url").description("The URL of the Entity"),
+        fieldWithPath("canonicalName").description("The canonical name for the Entity"),
+        fieldWithPath("notes").description("Notes describing the Entity"),
+        subsectionWithPath("_links").description("A list of links associated with the Entity")
+    );
+
+    private static final LinksSnippet ENTITY_COLLECTION_LINKS = links(
+        halLinks(),
+        linkWithRel("self").description("A link to the Entity collection"),
+        linkWithRel("profile").description("A link to the profile for the Entity collection"),
+        linkWithRel("findByUrl").description("Returns the Entity that either contains the given URL, or is associated with the Identifier that does"),
+        linkWithRel("findByName").description("Returns a list of Entities associated with a Name that matches the given value")
+    );
+
+    private static final LinksSnippet ENTITY_LINKS = links(
+        halLinks(),
+        linkWithRel("self").description("The canonical url for this Entity"),
+        linkWithRel("entity").description("A link to the Entity resource"),
+        linkWithRel("redirect").description("Redirects request to the URL associated with the Entity"),
+        linkWithRel("redirectByHead").description("Redirects HEAD requests to the URL associated with the Entity")
+    );
 
     static {
         try {
@@ -64,8 +97,12 @@ public class EntityCrudTest extends ApiDocumentation {
         entityRepo.save(TEST_ENTITY_1);
         entityRepo.save(TEST_ENTITY_2);
         this.mockMvc
-            .perform(get("/entity").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)
-                .param("page", "0").param("size", "25").param("sort", "desc"))
+            .perform(get("/entity")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("page", "0")
+                .param("size", "25")
+                .param("sort", "desc"))
             .andExpect(status().isOk())
             .andDo(document("get-all-entities",
                 requestParameters(
@@ -78,13 +115,7 @@ public class EntityCrudTest extends ApiDocumentation {
                     subsectionWithPath("_links").description("A list of links associated with the Enttiy collection"),
                     subsectionWithPath("page").description("A description of the attributes of the page")
                 ),
-                links(
-                    halLinks(),
-                    linkWithRel("self").description("A link to the Entity collection"),
-                    linkWithRel("profile").description("A link to the profile for the Entity collection"),
-                    linkWithRel("findByUrl").description("Returns the Entity that either contains the given URL, or is associated with the Identifier that does"),
-                    linkWithRel("findByName").description("Returns a list of Entities associated with a Name that matches the given value")
-                )
+                ENTITY_COLLECTION_LINKS
             ));
     }
 
@@ -93,55 +124,37 @@ public class EntityCrudTest extends ApiDocumentation {
         Entity entity = entityRepo.save(TEST_ENTITY_1);
         this.mockMvc
             .perform(get("/entity/{id}", entity.getId().toString())
-                .accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON))
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
+            .andExpect(jsonPath("id").value(entity.getId().toString()))
+            .andExpect(jsonPath("url").value(TEST_ENTITY_1_URL_STRING))
+            .andExpect(jsonPath("canonicalName").value(TEST_ENTITY_1_CANONICAL_NAME))
+            .andExpect(jsonPath("notes").value(TEST_ENTITY_1_NOTES))
             .andDo(document("get-entity",
                 pathParameters(
                     parameterWithName("id").description("The UUID id of the Entity to be retrieved")
                 ),
-                responseFields(
-                    fieldWithPath("id").description("The UUID id of the Entity"),
-                    fieldWithPath("url").description("The URL of the Entity"),
-                    fieldWithPath("canonicalName").description("The canonical name for the Entity"),
-                    fieldWithPath("notes").description("Notes describing the Entity"),
-                    subsectionWithPath("_links").description("A list of links associated with the Entity")
-                ),
-                links(
-                    halLinks(),
-                    linkWithRel("self").description("The canonical url for this Entity"),
-                    linkWithRel("entity").description("A link to the Entity resource"),
-                    linkWithRel("redirect").description("Redirects request to the URL associated with the Entity"),
-                    linkWithRel("redirectByHead").description("Redirects HEAD requests to the URL associated with the Entity")
-                )
+                ENTITY_RESPONSE_FIELDS_SNIPPET,
+                ENTITY_LINKS
             ));
     }
 
     @Test
     public void testCreateEntity() throws Exception {
         this.mockMvc
-            .perform(post("/entity").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)
+            .perform(post("/entity")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
                 .content(mapper.writeValueAsString(TEST_ENTITY_1)))
             .andExpect(status().isCreated())
+            .andExpect(jsonPath("url").value(TEST_ENTITY_1_URL_STRING))
+            .andExpect(jsonPath("canonicalName").value(TEST_ENTITY_1_CANONICAL_NAME))
+            .andExpect(jsonPath("notes").value(TEST_ENTITY_1_NOTES))
             .andDo(document("create-entity",
-                requestFields(
-                    fieldWithPath("url").description("The URL of the Entity"),
-                    fieldWithPath("canonicalName").description("The canonical name for the Entity"),
-                    fieldWithPath("notes").description("Notes describing the Entity")
-                ),
-                responseFields(
-                    fieldWithPath("id").description("The UUID id of the Entity"),
-                    fieldWithPath("url").description("The URL of the Entity"),
-                    fieldWithPath("canonicalName").description("The canonical name for the Entity"),
-                    fieldWithPath("notes").description("Notes describing the Entity"),
-                    subsectionWithPath("_links").description("A list of links associated with the Entity")
-                ),
-                links(
-                    halLinks(),
-                    linkWithRel("self").description("The canonical url for this Entity"),
-                    linkWithRel("entity").description("A link to the Entity resource"),
-                    linkWithRel("redirect").description("Redirects request to the URL associated with the Entity"),
-                    linkWithRel("redirectByHead").description("Redirects HEAD requests to the URL associated with the Entity")
-                )
+                ENTITY_REQUEST_FIELDS_SNIPPET,
+                ENTITY_RESPONSE_FIELDS_SNIPPET,
+                ENTITY_LINKS
             ));
     }
 
@@ -162,25 +175,9 @@ public class EntityCrudTest extends ApiDocumentation {
             pathParameters(
                 parameterWithName("id").description("The UUID id of the Entity to be replaced")
             ),
-            requestFields(
-                fieldWithPath("url").description("The URL of the Entity"),
-                fieldWithPath("canonicalName").description("The canonical name for the Entity"),
-                fieldWithPath("notes").description("Notes describing the Entity")
-            ),
-            responseFields(
-                fieldWithPath("id").description("The UUID id of the Entity"),
-                fieldWithPath("url").description("The URL of the Entity"),
-                fieldWithPath("canonicalName").description("The canonical name for the Entity"),
-                fieldWithPath("notes").description("Notes describing the Entity"),
-                subsectionWithPath("_links").description("A list of links associated with the Entity")
-            ),
-            links(
-                halLinks(),
-                linkWithRel("self").description("The canonical url for this Entity"),
-                linkWithRel("entity").description("A link to the Entity resource"),
-                linkWithRel("redirect").description("Redirects request to the URL associated with the Entity"),
-                linkWithRel("redirectByHead").description("Redirects HEAD requests to the URL associated with the Entity")
-            )
+            ENTITY_REQUEST_FIELDS_SNIPPET,
+            ENTITY_RESPONSE_FIELDS_SNIPPET,
+            ENTITY_LINKS
         ));
     }
 
@@ -201,25 +198,9 @@ public class EntityCrudTest extends ApiDocumentation {
             pathParameters(
                 parameterWithName("id").description("The UUID id of the Entity to be replaced")
             ),
-            requestFields(
-                fieldWithPath("url").description("The URL of the Entity"),
-                fieldWithPath("canonicalName").description("The canonical name for the Entity"),
-                fieldWithPath("notes").description("Notes describing the Entity")
-            ),
-            responseFields(
-                fieldWithPath("id").description("The UUID id of the Entity"),
-                fieldWithPath("url").description("The URL of the Entity"),
-                fieldWithPath("canonicalName").description("The canonical name for the Entity"),
-                fieldWithPath("notes").description("Notes describing the Entity"),
-                subsectionWithPath("_links").description("A list of links associated with the Entity")
-            ),
-            links(
-                halLinks(),
-                linkWithRel("self").description("The canonical url for this Entity"),
-                linkWithRel("entity").description("A link to the Entity resource"),
-                linkWithRel("redirect").description("Redirects request to the URL associated with the Entity"),
-                linkWithRel("redirectByHead").description("Redirects HEAD requests to the URL associated with the Entity")
-            )
+            ENTITY_REQUEST_FIELDS_SNIPPET,
+            ENTITY_RESPONSE_FIELDS_SNIPPET,
+            ENTITY_LINKS
         ));
     }
 
